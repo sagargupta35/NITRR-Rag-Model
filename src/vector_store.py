7dec8a20ed73aa1e00c60520c0d24ec25800a4fc
Sagar Gupta sagarguptha35@gmail.com <sagarguptha35@gmail.com>
2025-11-12 17:48:04 +0530

    model trained on ordinance forms

---
+++ src/vector_store.py
@@ -0,0 +1,13 @@
+import chromadb
+from chromadb.utils import embedding_functions
+from src.fs_utils import get_root_dir
+
+vector_dir_path = get_root_dir() / "data" / "vector_store_persistent_dir"
+client = chromadb.PersistentClient(vector_dir_path)
+
+embed_fn = embedding_functions.SentenceTransformerEmbeddingFunction(
+    model_name="all-MiniLM-L6-v2"
+)
+
+def get_collection(name: str):
+    return client.get_or_create_collection(name, embedding_function=embed_fn)
